8fec0a00fb3fc9e869fbf23036bbae32083b3120
mustafaevf <knetya274@gmail.com>
2023-04-04 22:20:09 +0300

    start concept

---
+++ app/main.php
@@ -0,0 +1,18 @@
+<?php 
+require_once('index.php');
+top('123');
+
+?>
+
+<header>
+    <div class="header-container">
+        <div class="header-logo">
+            asdasdas
+        </div>
+        <div class="header-menu">
+            <ul>
+                <li><a href="">dsadsa</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
